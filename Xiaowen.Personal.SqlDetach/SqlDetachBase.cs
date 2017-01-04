6d58c0fa1f1e96fb6043bb0a0ef6bbc1585d6569
zhangxiaowen <z.xiaowen@outlook.com>
2017-01-04 23:00:45 +0800

    added the required event

---
+++ Xiaowen.Personal.SqlDetach/SqlDetachBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Xiaowen.Personal.SqlDetach
+{
+    /// <summary>
+    /// author:xiaowen
+    /// </summary>
+    public class SqlDetachBase
+    {
+        /// <summary>
+        /// for ActionResult
+        /// </summary>
+        public event XwMvcActionHandler<ActionResult> XwMvcActionEvent;
+
+        /// <summary>
+        /// for ContentResult
+        /// </summary>
+        public event XwMvcContentHandler<ContentResult> XwMvcContentEvent;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void XwDoMvcActionResult(object sender, XwEventArgs e)
+        {
+            XwMvcActionEvent?.Invoke(sender, e);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void XwDoMvcActionResult(XwEventArgs e)
+        {
+            XwMvcActionEvent?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void XwDoMvcContentResult(object sender, XwEventArgs e)
+        {
+            XwMvcContentEvent?.Invoke(sender, e);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void XwDoMvcContentResult(XwEventArgs e)
+        {
+            XwMvcContentEvent?.Invoke(this, e);
+        }
+    }
+}
